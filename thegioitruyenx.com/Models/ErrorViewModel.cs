4dd1d653ab9c4a8e947b9a5a03cd6b25bc8511fc
Tú Nguyễn <tunguyen.itsys@gmail.com>
2020-04-13 13:54:08 +0700

    New project demo

---
+++ thegioitruyenx.com/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace thegioitruyenx.com.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
