44ccc5d6001d959cd0dad6a539d656fff4bb6693
Tú Nguyễn <tunguyen.itsys@gmail.com>
2020-04-14 08:23:42 +0700

    Design database

---
+++ thegioitruyenx.data/Entities/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace thegioitruyenx.data.Entities
+{
+    public class Product
+    {
+        public int Id { set; get; }
+        public decimal Price { set; get; }
+        public decimal OriginalPrice { set; get; }
+        public int Stock { set; get; }
+        public int ViewCout { set; get; }
+        public string DateCreate { set; get; }
+        public string SeoAlias { set; get; }
+    }
+}
---
+++ thegioitruyenx.data/EntityFrame/ThegioitruyenxDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using thegioitruyenx.data.Entities;
+
+namespace thegioitruyenx.data.EntityFrame
+{
+    public class ThegioitruyenxDbContext : DbContext
+    {
+        public ThegioitruyenxDbContext(DbContextOptions options) : base(options)
+        {
+            //options.UseSqlServer("");
+        }
+        public DbSet<Product> Products { set; get; }
+        public DbSet<Category> Categories { set; get; }
+    }
+}
